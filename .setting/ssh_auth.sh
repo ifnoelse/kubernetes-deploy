96a09616c916fae8be48df8ac1ec826889380732
zhangwei1 <zhangwei1@huan.tv>
2017-12-11 18:30:42 +0800

    初始化项目

--- .setting/ssh_auth.sh
+++ .setting/ssh_auth.sh
@@ -1,4 +1,7 @@
- su $1 -c "mkdir ~/.ssh"
- su $1 -c "cat /vagrant/.setting/public_key>>~/.ssh/authorized_keys"
- su $1 -c "chmod 700 ~/.ssh"
- su $1 -c "chmod 600 ~/.ssh/authorized_keys"
+#!/usr/bin/env bash
+su $1 -c "mkdir ~/.ssh"
+su $1 -c "cat /vagrant/.setting/public_key>>~/.ssh/authorized_keys"
+su $1 -c "chmod 700 ~/.ssh"
+su $1 -c "chmod 600 ~/.ssh/authorized_keys"
+su $1 -c "cat /vagrant/.setting/private_key>~/.ssh/id_dsa"
+su $1 -c "chmod 600 ~/.ssh/id_dsa"
