96d010f424b54fb0f4a2023959ad030c1b9d5bf4
zhangwei1 <zhangwei1@huan.tv>
2017-12-14 15:19:27 +0800

    更换秘钥

--- .setting/ssh_auth.sh
+++ .setting/ssh_auth.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 su $1 -c "mkdir ~/.ssh"
-su $1 -c "cat /vagrant/.setting/public_key>>~/.ssh/authorized_keys"
+su $1 -c "cat /vagrant/.setting/id_rsa.pub>>~/.ssh/authorized_keys"
 su $1 -c "chmod 700 ~/.ssh"
 su $1 -c "chmod 600 ~/.ssh/authorized_keys"
-su $1 -c "cat /vagrant/.setting/private_key>~/.ssh/id_dsa"
+su $1 -c "cat /vagrant/.setting/id_dsa>~/.ssh/id_dsa"
 su $1 -c "chmod 600 ~/.ssh/id_dsa"
 su $1 -c "echo 'StrictHostKeyChecking no'>~/.ssh/config"
